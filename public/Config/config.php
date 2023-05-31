71aee0288e0ecc6a29455d69646d44c6e66a9902
Slonexxx <21moriarty21@mail.ru>
2023-05-31 16:39:10 +0600

    new

--- public/Config/config.php
+++ public/Config/config.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'appId' => '672c9f92-0f5c-4eef-8ec1-1ea737be7515',
-    'appUid' => 'webkassa.smartinnovations',
-    'secretKey' => '0rhC5i8gdbSqJ3KnoBjZfci3hnJseq0mGb0QdqEu3PiIM4wp5loQCRMIfIMK5AR5yPJ2OvApvzCzQ9Z81pbYJpYWkp9LlsvvUW8c47q1wJocUZW7GeNxGAqE6AR98RX5',
-    'appBaseUrl' => 'https://smartwebkassa.kz/',
+    'appId' => '104b8469-fb0f-41b5-b03f-61ceed1d3590',
+    'appUid' => 'esf.smartinnovations',
+    'secretKey' => 'U4s8bCGjc8digSBsuqLlVtK3b5aMS62Jr7TkTyzYJJMwLTfopxWChSOLETMrCti8SRy0guzyX5NoYkjea6VLfFjSYpNs2ftpCabxLjKSCSrjBtmSMFgTK6GnwGciVDgU',
+    'appBaseUrl' => 'https://smartesf.kz/',
 ];
